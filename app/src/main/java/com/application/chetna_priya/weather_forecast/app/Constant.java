7265335c6d781894cf75a798fcd4ae457e005da4
chetna <chetna.priya@gmail.com>
2016-05-10 10:49:08 -0600

    Remove api key from constant

--- app/src/main/java/com/application/chetna_priya/weather_forecast/app/Constant.java
+++ app/src/main/java/com/application/chetna_priya/weather_forecast/app/Constant.java
@@ -5,6 +5,6 @@ package com.application.chetna_priya.weather_forecast.app;
  */
 public class Constant {
 
-    public static final String WEATHER_API_KEY = "f57b059cdfe3fdee3d94b98041800d91";
+    public static final String WEATHER_API_KEY = "";
     public static final int DEFAULT_LOC_LENGTH = 5;
 }
